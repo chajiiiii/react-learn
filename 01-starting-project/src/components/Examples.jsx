4c45bd596ce90dd30f7975e5845de9dfbc29abc2
chajiiiii <j.cha1708@gmail.com>
2025-12-01 23:37:05 +0900

    learn. forwarding props to inner elements

--- 01-starting-project/src/components/Examples.jsx
+++ 01-starting-project/src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import TabButton from "./TabButton";
 import { useState } from "react";
 import { EXAMPLES } from "../data";
+import Section from "./Section";
 
 export default function Examples() {
   const [selectedTopic, setSelectedTopic] = useState();
@@ -16,30 +17,29 @@ export default function Examples() {
   }
 
   return (
-    <section id="examples">
-      <h2>EXAMPLES</h2>
+    <Section title="Examples" id="examples">
       <menu>
         <TabButton
           isSelected={selectedTopic === "components"}
-          onSelect={() => handleClick("components")}
+          onClick={() => handleClick("components")}
         >
           COMPONENTS
         </TabButton>
         <TabButton
           isSelected={selectedTopic === "jsx"}
-          onSelect={() => handleClick("jsx")}
+          onClick={() => handleClick("jsx")}
         >
           JSX
         </TabButton>
         <TabButton
           isSelected={selectedTopic === "props"}
-          onSelect={() => handleClick("props")}
+          onClick={() => handleClick("props")}
         >
           PROPS
         </TabButton>
         <TabButton
           isSelected={selectedTopic === "state"}
-          onSelect={() => handleClick("state")}
+          onClick={() => handleClick("state")}
         >
           STATE
         </TabButton>
@@ -57,6 +57,6 @@ export default function Examples() {
           </pre>
         </div>
       )}
-    </section>
+    </Section>
   );
 }
