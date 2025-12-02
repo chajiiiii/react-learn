9019744a61c8baa4e42e55429cea656e306a4d1b
chajiiiii <j.cha1708@gmail.com>
2025-12-03 00:04:18 +0900

    learn. dynamic component types as custom components

--- 01-starting-project/src/components/Examples.jsx
+++ 01-starting-project/src/components/Examples.jsx
@@ -33,9 +33,9 @@ export default function Examples() {
     <Section title="Examples" id="examples">
       <Tabs
         // 컨테이너 타입을 다이나믹하게 하고 싶을 때.
-        buttonsContainer="menu"
+        ButtonsContainer="menu"
         // buttonsContainer={SectionComponent}
-        // string 대신 컴포넌트도 적용 가능
+        // string 대신 커스텀 컴포넌트도 적용 가능
         buttons={
           <>
             <TabButton
--- 01-starting-project/src/components/Tabs.jsx
+++ 01-starting-project/src/components/Tabs.jsx
@@ -1,6 +1,9 @@
-export default function Tabs({ children, buttons, buttonsContainer }) {
-  const ButtonsContainer = buttonsContainer;
+export default function Tabs({ children, buttons, ButtonsContainer }) {
+  // const ButtonsContainer = buttonsContainer;
   // 변수에 컴포넌트처럼 대문자로 저장을 해야 쓸 수 있음
+
+  // 또는 인수에서부터 프롭을 커스텀 컴포넌트처럼 대문자로 설정하여 받으면
+  // 변수에 따로 저장하지 않고 사용 가능
 
   return (
     <>
